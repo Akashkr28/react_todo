7a5ef879d21610cdd130f91b0a399a577fcd1464
Akashkr28 <email.akash2010@gmail.com>
2024-08-29 21:46:38 +0530

    small error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
+import TodoList from './component/TodoList/TodoList'
 
 function App() {
 
+  const todos = [
+    {id: 1, text: 'todo 1', isFinished: true},
+    {id: 2, text: 'todo 2', isFinished: false},
+  ]
+
   return (
-    <>Todos</>
+    <>
+    <TodoList todos={todos}/>
+    </>
   )
 }
 
