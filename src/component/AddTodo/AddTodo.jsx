e42e5a920818083286ff94776dcfda64167914ea
Akashkr28 <email.akash2010@gmail.com>
2024-08-31 20:54:11 +0530

    implementing and knowing context api

--- src/component/AddTodo/AddTodo.jsx
+++ src/component/AddTodo/AddTodo.jsx
@@ -1,6 +1,9 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
+import TodoContext from "../../context/TodoContext";
 
-function AddTodo({addTodos}){
+function AddTodo({}){
+
+    const { todos, setTodos } = useContext(TodoContext)
 
     const [todoText, setTodoText] = useState('')
 
@@ -10,9 +13,11 @@ function AddTodo({addTodos}){
                 placeholder="add your next todo...."
                 onChange={(e) => setTodoText(e.target.value)}
                 value={todoText} />
+                
             <button onClick={() => {
-                addTodos(todoText);
-                setTodos(['']);
+                let nextId = todos.length + 1;
+                setTodos([...todos, {id: nextId, text: todoText, isFininsed: false}]);
+                setTodoText('');
             }}>Submit</button>
         </>
     )
